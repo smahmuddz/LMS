dcf73431f2846d2937327c9dd5098a9d3b63ada6
Sabbir Mahmud Afridi <smahmuddz@gmail.com>
2022-01-07 18:10:50 +0600

    Update Book issue.sql

--- Book issue.sql
+++ Book issue.sql
@@ -1,53 +1,23 @@
-declare
-B_ID number(7);
-Bo_ID number(7);
-FINE number(3);
-B_IS_ID number(7);
+Declare
+user_id number(10);
+
 begin
+	if(:P10_USER_TYPE = 'FACULTY') then
+		user_id := :P10_BORROWER_FACULTY;
+          insert into LMS_BOOK_ISSUE(BOOK_ISSUE_ID,BOOK_ID,BORROWER_ID,ISSUED_BY,BORROWED_FROM_DATE,BORROWED_TO_DATE,BORROWER_TYPE,BOOK_RT_STATUS)
+    values(LMS_BOOK_ISSUE_SEQ.nextval,:P10_BOOK_TITLE,user_id, 1, :P10_BOROWED_DATE,:P10_RETURN_DATE,'FACULTY',1 );
     
-    if(:P11_BOOK_RETURN_FORM = 'FACULTY') then
-                select BOOK_ID into B_ID from LMS_BOOK_ISSUE where BOOK_ISSUE_ID =  :P11_BOOK_ISSUE_ID_FAC;
-                
-                select BORROWER_ID into Bo_ID from LMS_BOOK_ISSUE where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_FAC;
-                
-                select 10 * (to_number(to_date (sysdate,'MM-DD-YYYY') - to_date(BORROWED_TO_DATE,'MM-DD-YYYY'))) into FINE from LMS_BOOK_ISSUE where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_FAC;
-                if(FINE>0) then
-                :P11_DUE_FINE_FACULTY := FINE;
-                else
-                FINE:=0;
-                :P11_DUE_FINE_FACULTY := FINE;
-                end if;
-                
-                insert into LMS_BOOK_RETURN(BOOK_RETURN_ID,BOOK_ID,BORROWER_ID,BOOK_ISSUE_ID,RETURN_DATE,FINE)
-                values(LMS_BOOK_RETURN_SEQ.nextval, B_ID,Bo_ID,:P11_BOOK_ISSUE_ID_FAC,sysdate, FINE  );
-                
-                update LMS_BOOK_DETAIL set BOOK_NO_OF_COPIES_CURRENT = BOOK_NO_OF_COPIES_CURRENT+ 1 where BOOK_ID= B_ID;
-                
-                update LMS_BOOK_ISSUE set BOOK_RT_STATUS = 0 where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_FAC;
-                
-                
-     else
-                select BOOK_ID into B_ID from LMS_BOOK_ISSUE where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_STU;
-              
-                select BORROWER_ID into Bo_ID from LMS_BOOK_ISSUE where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_STU;
-                             
-                select 10 * (to_number(to_date (sysdate,'MM-DD-YYYY') - to_date(BORROWED_TO_DATE,'MM-DD-YYYY')))  into FINE from LMS_BOOK_ISSUE where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_STU;
-                if(FINE>0) then
-                :P11_DUE_FINE_STUDENT := FINE;
-                else
-                FINE:=0;
-                :P11_DUE_FINE_STUDENT := FINE;
-                end if;
-                
-                
-                 insert into LMS_BOOK_RETURN(BOOK_RETURN_ID,BOOK_ID,BORROWER_ID,BOOK_ISSUE_ID,RETURN_DATE,FINE)
-                values(LMS_BOOK_RETURN_SEQ.nextval, B_ID,Bo_ID,:P11_BOOK_ISSUE_ID_STU,sysdate, FINE  );
-                
-                update LMS_BOOK_DETAIL set BOOK_NO_OF_COPIES_CURRENT = BOOK_NO_OF_COPIES_CURRENT+ 1 where BOOK_ID= B_ID;
-                
-                update LMS_BOOK_ISSUE set BOOK_RT_STATUS = 0 where BOOK_ISSUE_ID = :P11_BOOK_ISSUE_ID_STU;
-               
-     end if;
-   /* insert into LMS_BOOK_RETURN(BOOK_RETURN_ID,BOOK_ID,BORROWER_ID,BOOK_ISSUE_ID,RETURN_DATE,FINE)
-    values(1,1,1,1,'1-1-2000',20);*/
+    update LMS_BOOK_DETAIL set BOOK_NO_OF_COPIES_CURRENT = BOOK_NO_OF_COPIES_CURRENT-1 where BOOK_ID=:P10_BOOK_TITLE;
+    
+	else
+		user_id := :P10_STUDENT;
+        insert into LMS_BOOK_ISSUE(BOOK_ISSUE_ID,BOOK_ID,BORROWER_ID,ISSUED_BY,BORROWED_FROM_DATE,BORROWED_TO_DATE,BORROWER_TYPE,BOOK_RT_STATUS)
+    values(LMS_BOOK_ISSUE_SEQ.nextval,:P10_BOOK_TITLE,user_id, 1, :P10_BOROWED_DATE,:P10_RETURN_DATE,'STUDENT',1 );
+    
+    update LMS_BOOK_DETAIL set BOOK_NO_OF_COPIES_CURRENT = BOOK_NO_OF_COPIES_CURRENT-1 where BOOK_ID=:P10_BOOK_TITLE;
+    
+	end if;
+    
+    
+
 end;
